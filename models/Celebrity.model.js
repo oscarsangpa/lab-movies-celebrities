3bb07b332b15e0837878a945e905dd13696d9517
Oscar Sangrador <oscarsangrador@gmail.com>
2022-02-01 20:56:42 +0100

    iteration 1-2 done

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,26 @@
 //  Add your code here
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const schema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Name is required'],
+    },
+    occupation: {
+      type: String,
+      default: 'unknown'
+    },
+    catchPhrase: {
+      type: String,
+      required: [true, 'CatchPhrase is required'],
+    }
+  },
+  { timestamps: true },
+);
+
+const Celebrity = mongoose.model('Celebrity', schema);
+
+module.exports = Celebrity;
